4157e45b908250fc0dd5a7938fcaf1f29551b0a6
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-02-01 10:15:19 -0300

    declare_request_example_data complete

---
+++ declare_request_example_data/main.py
@@ -0,0 +1,52 @@
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+from typing import Annotated
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str = Field(examples=["Foo"])
+    description: str | None = Field(default=None, examples=["A very nice Item"])
+    price: float = Field(examples=[35.4])
+    tax: float | None = Field(default=None, examples=[3.2])
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        )   
+    ]
+):
+    results = {"item_id": item_id, "item": item}
+    return results
